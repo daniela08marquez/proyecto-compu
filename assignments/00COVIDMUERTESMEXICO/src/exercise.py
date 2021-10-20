bdac82a064b95be6961ed41fda0b4a88061b39f2
a01740729 <a01740729@itesm.mx>
2021-10-20 19:02:51 +0000

    dkvcn

--- assignments/00COVIDMUERTESMEXICO/src/exercise.py
+++ assignments/00COVIDMUERTESMEXICO/src/exercise.py
@@ -1,12 +1,30 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
-
+    reg = si 
     nomb = input('¿Cual es tu nombre? ')
     print ('Hola ' + str(nomb))
     print ('Este programa prensetara datos sobre las muertes por covid en México desde Marzo 18 2020 hasta Octubre 18 2021')
     print (' ')
-    print ('Seleccione la opcion que ')
+    while (reg == si): 
+        print ('Seleccione de las siguientes opciones')
+    
+        print ('1.- Muertes totales en México de Marzo hasta Octubre.')
+        print ('2.- Muertes totales por estado.')
+        print ('3.- Grafica de muertes entre hombres y mujeres')
+        print ('4.- Grafica de muertes entre tipo de paciente')
+        print ('5.- Cree tener sintomas de covid')
+    
+        opc = input (int('Selecione su opción'))
+
+        if opc == 1:
+            print ('En méxico hay un total de 287,477 mil muertes acumuladas a causa de covid')
+            print ('¿Desea volver al menu principal?')
+            reg = input ('')
+        elif opc == 2:
+            print ('hjkghngk')
+        else:
+            print ('sdnjfns')
     
 
 
