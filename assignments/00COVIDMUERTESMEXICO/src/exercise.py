16802fd59b9ec359a8d874be117dafcc262541f4
a01740729 <a01740729@itesm.mx>
2021-10-20 00:45:40 +0000

    cv

--- assignments/00COVIDMUERTESMEXICO/src/exercise.py
+++ assignments/00COVIDMUERTESMEXICO/src/exercise.py
@@ -2,9 +2,11 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
-    nomb = int (str ('¿Cual es tu nombre? '))
-    print ('Hola' + str(nomb))
+    nomb = input('¿Cual es tu nombre? ')
+    print ('Hola ' + str(nomb))
     print ('Este programa prensetara datos sobre las muertes por covid en México desde Marzo 18 2020 hasta Octubre 18 2021')
+    print (' ')
+    print ('Seleccione la opcion que ')
     
 
 
